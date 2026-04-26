c76c2f7d85a3b55875e0f63aa31107137426c0cc
BD-Personal <44916264+BD-Personal@users.noreply.github.com>
2026-04-25 19:07:54 -0700

    init crawler changes
    modified a little bit of scraper, changed config

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
 from urllib.parse import urlparse
 
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -15,7 +16,35 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    MAX_LENGTH = 10000 # used to check content size max
+
+    set_links = set()
+
+    for link in resp.raw_response.content:
+        
+        # if link is equal to webpage itself, ignore it
+        if (url == link):
+            continue
+
+
+        # possible issues: loading entire content stream may crash
+        # try using lambda to parse
+        if (len(resp.raw_response.content) > MAX_LENGTH):
+            continue
+
+
+
+        # strip the fragment off of the url and add it to the set (final check)
+        if is_valid(link):
+            next_links.add(urllib.parse.urldefrag(link).url)
+
+
+    return list(set_links)
+
+
+
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -25,15 +54,27 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+
+        
+        if re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+           + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+            return False
+
+        if not re.match(
+            r".*\.(ics.uci.edu/|cs.uci.edu/|informatics.uci.edu/|stat.uci.edu/)$", 
+            parsed.hostname.lower()):
+            return False
+
+        return True
+
+
 
     except TypeError:
         print ("TypeError for ", parsed)
